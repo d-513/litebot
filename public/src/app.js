920283e4b02ac7ff3b4130f907095e7e36fbb926
dada513 <dada513@protonmail.com>
2021-05-05 20:00:45 +0200

    Revert "Start working on web dashboard"
    This reverts commit d31ce8b251f9641145d6b179b00061ff47d19682.

--- public/src/app.js
+++
@@ -1,9 +0,0 @@
-import { html, LitElement } from "lit-element";
-
-class AppMain extends LitElement {
-  render() {
-    return html`<h1>test</h1>`;
-  }
-}
-
-customElements.define("app-main", AppMain);
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  entry: "./public/src/app.js",
-  output: {
-    filename: "dist/0.js",
-    path: path.resolve(__dirname, "public"),
-  },
-};
